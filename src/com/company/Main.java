7782a1968e5782910a2c70a3c5e0b005570a55f6
Karlas <karolis.zagarnauskas@gmail.com>
2018-01-31 16:47:36 +0200

    first commit

--- src/com/company/Main.java
+++ src/com/company/Main.java
@@ -1,8 +1,30 @@
 package com.company;
 
+import java.util.Arrays;
+import java.util.Scanner;
+
 public class Main {
 
     public static void main(String[] args) {
-	// write your code here
+        Scanner sc = new Scanner(System.in);
+        System.out.println("Iveskite kiek norite skaiciu ivesti");
+        int a = sc.nextInt();
+        int[] masyvas = new int[a];
+        for (int i = 0; i < masyvas.length; i++) {
+            System.out.println("Iveskite skaiciu");
+            int skaicius = sc.nextInt();
+            masyvas[i] = skaicius;
+        }
+        atvaizdavimas(masyvas);
+    }
+
+    public static void atvaizdavimas(int[] masyvas) {
+        for (int i = 0; i < masyvas.length; i++) {
+            if (masyvas[i] > 100) {
+                System.out.print(masyvas[i]+ " ");
+            }
+        }
+
     }
 }
+
